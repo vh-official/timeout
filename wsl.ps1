1b0b4513b87bc0b1c0a144fcea381386c2315b42
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2021-10-14 17:11:59 +0700

    Create wsl.ps1

---
+++ wsl.ps1
@@ -0,0 +1,3 @@
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
