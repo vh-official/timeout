1591aca9fac25b6ceec262e793ea78b15608edf2
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2021-10-14 17:59:09 +0700

    Update wsl.ps1

--- wsl.ps1
+++ wsl.ps1
@@ -2,3 +2,4 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 Invoke-WebRequest https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl.msi
 ./wsl.msi
+wsl.exe --install -d Ubuntu-20.04
