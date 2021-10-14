8d550caaab355076780a8c4c5d1dad65b1d84d5f
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2021-10-14 17:31:37 +0700

    Update wsl.ps1

--- wsl.ps1
+++ wsl.ps1
@@ -1,3 +1,4 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 Invoke-WebRequest https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl.msi
+./wsl.msi
